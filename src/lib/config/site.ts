217597a3d31a848e9db8545d51aaddeedbaf229c
Seth Howell <SethDoesDev@gmail.com>
2023-02-08 12:07:59 -0800

    updated site config and profile text

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,16 +2,15 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'earmarkspod.com',
+  title: 'Earmarks',
+  subtitle: 'Out-of-print book reviews',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: 'this podcast is a pod.punk production',
   author: {
-    avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    avatar: '/assets/EarmarksLogo-V1.png',
+    name: 'Earmarks Podcast',
+    bio: 'A podcast reviewing out-of-print and rare books that you SHOULD read!'
   },
   themeColor: '#3D4451'
 }
