d745ed71bc27679799cfdccb91dea2fbb1b5a9f9
Seth Howell <SethDoesDev@gmail.com>
2023-02-08 00:41:32 -0800

    updated nav and added episode page

--- src/lib/config/general.ts
+++ src/lib/config/general.ts
@@ -56,12 +56,12 @@ export const head: HeadConfig = {}
 export const header: HeaderConfig = {
   nav: [
     {
-      text: 'Get Started',
-      link: '/hello-world'
+      text: 'Episodes',
+      link: '/episodes'
     },
     {
-      text: 'Elements',
-      link: '/hello-world/elements'
+      text: 'About',
+      link: '/about'
     }
   ]
 }
