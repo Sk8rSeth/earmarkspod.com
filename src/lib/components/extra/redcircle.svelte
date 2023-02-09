5fb55a386bd383b2545602281e329bbc127c51fe
Seth Howell <SethDoesDev@gmail.com>
2023-02-08 18:34:18 -0800

    added redcircle player

---
+++ src/lib/components/extra/redcircle.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let id: string
+    const src = `https://redcircle.com/embedded-player/sh/bf11c8f9-3c8e-4125-9a8a-6b36e97c1379/ep/${id}`
+  </script>
+  
+  <iframe
+  {src}
+  class="redcircle-player"
+  title="RedCircle Episode Player"
+  frameborder="0"
+  loading="lazy"
+  scrolling="no"
+  height="170"
+  width="750"
+  style="border: none; max-width: 700px;"
+  allowfullscreen />
